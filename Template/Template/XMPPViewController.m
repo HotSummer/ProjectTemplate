5e3127d7d26842213c43a2be0c6d260090bfacd6
summer.zhu <summer.zhu@vipshop.com>
2015-06-25 10:48:23 +0800

    name

--- Template/Template/XMPPViewController.m
+++ Template/Template/XMPPViewController.m
@@ -20,7 +20,8 @@
 
 - (IBAction)didPressedBtnConnect:(id)sender {
     [[XMPPHelper shareInstance] setupStream];
-    [[XMPPHelper shareInstance] connect:@"nxjwalqyh@182.254.153.66/XMPPIOS" password:@"123456" willConnect:^(NSString *errorDescription, XMPPStream *xmppStream) {
+    
+    [[XMPPHelper shareInstance] connect:@"nxjwalqyh" password:@"123456" willConnect:^(NSString *errorDescription, XMPPStream *xmppStream) {//nxjwalqyh@182.254.153.66/XMPPIOS
         NSLog(@"%@", xmppStream);
     } didConnect:^(NSString *errorDescription, XMPPStream *xmppStream) {
         if (errorDescription.length > 0) {
